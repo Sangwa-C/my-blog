08975307b975e83e5f0ae118d45204e8565473d0
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-09-14 09:56:23 +0200

    my blog project

---
+++ src/main/java/App.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+import models.Post;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //post: process new post form
+        post("/posts/new", (request, response) -> { //URL to make new post on POST route
+            Map<String, Object> model = new HashMap<>();
+
+            String content = request.queryParams("content");
+            Post newPost = new Post(content);
+            model.put("post", newPost);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //get: show all posts
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Post> posts = Post.getAll();
+            model.put("posts", posts);
+
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //get: show an individual post
+
+        //get: show a form to update a post
+
+        //post: process a form to update a post
+
+        //get: delete an individual post
+
+        //get: delete all posts
+
+    }
+}
---
+++ src/main/java/models/Post.java
@@ -0,0 +1,49 @@
+package models;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class Post {
+    private final String content;
+    private static ArrayList<Post> instances = new ArrayList<>();
+    private boolean published;
+    private LocalDateTime createdAt;
+    private int id;
+
+    public Post (String content){
+        this.content = content;
+        this.published = false;
+        this.createdAt = LocalDateTime.now();
+        instances.add(this);
+        this.id = instances.size();
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public static ArrayList<Post> getAll(){
+        return instances;
+    }
+
+    public static void clearAllPosts(){
+        instances.clear(); //clear as a method is part of the ArrayList class.
+    }
+
+    public boolean getPublished(){
+        return this.published;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static Post findById(int id){
+        return instances.get(id-1); //why minus 1? See if you can figure it out.
+    }
+
+
+}
